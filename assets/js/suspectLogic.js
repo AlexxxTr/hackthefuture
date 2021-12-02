1dbb863140660ec24a4a1b2904f99a45bf385340
Ilya Dierckens <ilya.dierckens@student.howest.be>
2021-12-02 12:47:08 +0100

    Added motive suspect to json

--- assets/js/suspectLogic.js
+++ assets/js/suspectLogic.js
@@ -1,16 +1,23 @@
 
 async function createCompleteSuspectsjson(allSuspects){
     console.log(allSuspects);
-    let allCars;
-    await fetch("https://htf-2021.zinderlabs.com/car", requestOptions)
-    .then(response => response.json())
-    .then(result => allCars = result)
+    let allCars = await fetchFromApi("car")
+    let allMotives = await fetchFromApi("motive")
 
     for (let suspect of allSuspects){
-        suspect.car = getCarSuspect(suspect.name, allCars);
+        suspect.car = allCars.find(i => i.owner == suspect.name);
+        suspect.motive = allMotives.find(i => i.suspectId == suspect.id)?.text;
     }
+
 }
 
-function getCarSuspect(suspectName, allCars){
-    return allCars.find(i => i.owner == suspectName)
+async function fetchFromApi(endpoint){
+    let res;
+    await fetch("https://htf-2021.zinderlabs.com/" + endpoint, requestOptions)
+    .then(response => response.json())
+    .then(result => res = result)
+    return res;
 }
+
+
+
