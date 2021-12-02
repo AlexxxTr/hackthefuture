8196ac2361c61cc127511da47d34bb43846e5655
Ilya Dierckens <ilya.dierckens@student.howest.be>
2021-12-02 10:45:49 +0100

    Added base structure for suspect rendering

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,3 +13,15 @@ const requestOptions = {
 function index(){
     renderSuspects()
 }
+
+function renderSuspects(){
+    fetch("https://htf-2021.zinderlabs.com/suspect", requestOptions)
+    .then(suspects => function(suspects){
+        suspectContainer = document.querySelector("#suspects")
+        for(suspect in suspects){
+            suspectContainer.innerHTML += `
+                
+            `;
+        }
+    }).catch(err => console.log('error', err));
+}
