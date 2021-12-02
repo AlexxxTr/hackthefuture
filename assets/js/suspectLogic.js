439598c6cc02fe05d6a0c2b59e5c46abd7b05adc
Ilya Dierckens <ilya.dierckens@student.howest.be>
2021-12-02 12:59:16 +0100

    Added sighting car

--- assets/js/suspectLogic.js
+++ assets/js/suspectLogic.js
@@ -2,13 +2,26 @@
 async function createCompleteSuspectsjson(allSuspects){
     console.log(allSuspects);
     let allCars = await fetchFromApi("car")
+    console.log(allCars);
     let allMotives = await fetchFromApi("motive")
 
     for (let suspect of allSuspects){
         suspect.car = allCars.find(i => i.owner == suspect.name);
+        if (suspect.car){
+            suspect.car.sighting = await fetchFromApi("sighting/" + "car/" + suspect.car.licenseplate);
+        } 
         suspect.motive = allMotives.find(i => i.suspectId == suspect.id)?.text;
     }
+    console.log(allSuspects);
+}
 
+function calculateSuspectSuspiciousness(suspect){
+    let suspiciousness = 0;
+    if (suspect.motive == undefined){
+        suspiciousness =-3;
+    } else {
+        suspiciousness += 5;
+    }
 }
 
 async function fetchFromApi(endpoint){
